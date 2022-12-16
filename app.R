792f9f659d10c5551c6a86a1292faf81c0e1a5a9
mario <azulmario@yahoo.com.mx>
2022-12-16 11:36:37 -0600

    La selección de desagregación se cambia a radio

--- app.R
+++ app.R
@@ -5,19 +5,40 @@ library(shinyWidgets) # 0.7.5
 library(tidyverse) # 1.3.1
 source("global.R")
 
-#todo@ BUG hay un error al desplegar la barra de tiempo en remesas, tercer indicador.
 #todo@ TEST probar con diferentes niveles de desagregación geográfica
 
 server <- function(input, output, session) {
+
+  observeEvent(input$selColeccion, {
+    actualiza_indicador(input$selColeccion)
+    updateSelectInput(
+      session = session,
+      inputId = "selIndicador",
+      choices = opciones_indicadores,
+      selected = NULL
+    )
+    actualiza_bd(indicadores[1, 1])
+    updateRadioButtons(inputId = "selEnt",
+                      label = "Desagregación:",
+                      choices = opciones_entidad)
+  }, ignoreInit = TRUE)
+  
+  observeEvent(input$selIndicador, {
+    actualiza_bd(input$selIndicador)
+    updateRadioButtons(inputId = "selEnt",
+                label = "Desagregación:",
+                choices = opciones_entidad)
+  }, ignoreInit = TRUE)
+
   #Sugerencia: usar shinywidgets::sliderTextInput
   output$sldAnio <- renderUI({
     ad <- anios_disponibles(input$selIndicador)
     if(length(ad) > 1) {
       sliderTextInput(inputId = "sldAnio",
-                      label = "Seleccione año:",
+                      label = "Año:",
                       grid = TRUE,
                       choices = ad,
-                      selected = ad[1])
+                      selected = ad[length(ad)])
     } else if (length(ad) > 0) {
       selectInput(inputId = "sldAnio",
                   label = "Año",
@@ -28,7 +49,7 @@ server <- function(input, output, session) {
       br()
     }
   })
-  
+
   output$grafica_barras <- renderPlot({
     gen_barras(edo_sel = input$selEnt,
                ind_sel = input$selIndicador,
@@ -36,60 +57,41 @@ server <- function(input, output, session) {
     
   })
   
-   output$mapa <- renderPlot({
-     gen_mapa(edo_sel = input$selEnt,
-              ind_sel = input$selIndicador,
-              anio_sel = input$sldAnio)
+  output$mapa <- renderPlot({
+    gen_mapa(edo_sel = input$selEnt,
+             ind_sel = input$selIndicador,
+             anio_sel = input$sldAnio)
     
   })
   
   output$grafica_lineas <- renderPlotly({
-     gen_lineas(edo_sel = input$selEnt,
-                ind_sel = input$selIndicador,
-                anio_sel = input$sldAnio)
+    gen_lineas(edo_sel = input$selEnt,
+               ind_sel = input$selIndicador,
+               anio_sel = input$sldAnio)
   })
-  
-  observeEvent(input$selColeccion, {
-    actualiza_indicador(input$selColeccion)
-    updateSelectInput(
-      session = session,
-      inputId = "selIndicador",
-      choices = opciones_indicadores,
-      selected = NULL
-    )
-    actualiza_bd(indicadores[1, 1])
-    updateSelectInput(inputId = "selEnt",
-                      label = "Seleccione nivel de desagregación:",
-                      choices = opciones_entidad)
-  }, ignoreInit = TRUE)
-  
-  observeEvent(input$selIndicador, {
-    actualiza_bd(input$selIndicador)
-    updateSelectInput(inputId = "selEnt",
-                label = "Seleccione nivel de desagregación:",
-                choices = opciones_entidad)
-  }, ignoreInit = TRUE)
-  
+
 }
 
 ui <- fluidPage(
   h1("Catálogo de indicadores", style = "color:rgb(0,167,225);"),
-  br(),
+  #br(),
   sidebarLayout(
     sidebarPanel(
       #      h2("Controles"),
       selectInput(inputId = "selColeccion",
-                  label = "Seleccione colección:",
+                  label = "Colección:",
                   choices = opciones_coleccion,
                   selected = opciones_coleccion[1]),
       selectInput(inputId = "selIndicador",
-                  label = "Seleccione indicador:",
+                  label = "Indicador:",
                   choices = opciones_indicadores,
                   selected = opciones_indicadores[1]),
-      selectInput(inputId = "selEnt",
-                  label = "Seleccione nivel de desagregación:",
+      radioButtons(inputId = "selEnt",
+                  label = "Desagregación:",
                   choices = opciones_entidad),
-      uiOutput("sldAnio")
+      uiOutput("sldAnio"),
+      br(),
+      helpText("© 2023 Gobierno del Estado de Guanajuato")
     ),
     mainPanel(
       tabsetPanel(
