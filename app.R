36c786337a127b5f91a055f4581a5e2c247d0a62
mario <azulmario@yahoo.com.mx>
2022-12-22 11:58:31 -0600

    Organiza el código con estilo

--- app.R
+++ app.R
@@ -6,9 +6,62 @@ library(tidyverse) # 1.3.1
 source("global.R")
 
 #todo@ TEST probar con diferentes niveles de desagregación geográfica
+# Agregar opciones en panel:
+# [*] Ocultar total
+# [*] Resaltar Guanajuato
+# Agregar pestañas
+# [-] Tabulado
+# [-] Prospectiva (Si hay más de 3 datos para Guanajuato)
+# [-] Metadatos
+
+ui <- fluidPage(
+  includeCSS("www/styles.css"),
+  titlePanel("Catálogo de indicadores"),
+  #br(),
+  sidebarLayout(
+    sidebarPanel(
+      # Controles
+      selectInput(inputId = "selColeccion",
+                  label = "Colección:",
+                  choices = opciones_coleccion,
+                  selected = opciones_coleccion[1]),
+      selectInput(inputId = "selIndicador",
+                  label = "Indicador:",
+                  choices = opciones_indicadores,
+                  selected = opciones_indicadores[1]),
+      radioButtons(inputId = "selEnt",
+                  label = "Desagregación:",
+                  choices = opciones_entidad),
+      uiOutput("sldAnio"),
+      hr(),
+      checkboxGroupInput("checkGroup", label = "Opciones:", 
+                         choices = list("Ocultar total" = 1, "Resaltar Guanajuato" = 2),
+                         selected = c(1, 2)),
+      hr(),
+      helpText("© 2023 Gobierno del Estado de Guanajuato")
+    ),
+    mainPanel(
+      tabsetPanel(
+        tabPanel(title = "Tabulado",
+                 br()),
+        tabPanel(title = "Gráfica",
+                 plotOutput("grafica_barras", height = "80vh") %>% withSpinner()),
+        tabPanel(title = "Mapa",
+                 plotOutput("mapa", height = "80vh") %>% withSpinner()),
+        tabPanel(title = "Serie",
+                 plotOutput("grafica_lineas", height = "80vh") %>% withSpinner()),
+        tabPanel(title = "Prospectiva",
+                 br()),
+        tabPanel(title = "Metadato",
+                 br(),
+                 icon = icon("book"))
+      )
+    )
+  )
+)
 
 server <- function(input, output, session) {
-
+  
   observeEvent(input$selColeccion, {
     actualiza_indicador(input$selColeccion)
     updateSelectInput(
@@ -19,17 +72,17 @@ server <- function(input, output, session) {
     )
     actualiza_bd(indicadores[1, 1])
     updateRadioButtons(inputId = "selEnt",
-                      label = "Desagregación:",
-                      choices = opciones_entidad)
+                       label = "Desagregación:",
+                       choices = opciones_entidad)
   }, ignoreInit = TRUE)
   
   observeEvent(input$selIndicador, {
     actualiza_bd(input$selIndicador)
     updateRadioButtons(inputId = "selEnt",
-                label = "Desagregación:",
-                choices = opciones_entidad)
+                       label = "Desagregación:",
+                       choices = opciones_entidad)
   }, ignoreInit = TRUE)
-
+  
   #Sugerencia: usar shinywidgets::sliderTextInput
   output$sldAnio <- renderUI({
     ad <- anios_disponibles(input$selIndicador)
@@ -49,7 +102,7 @@ server <- function(input, output, session) {
       br()
     }
   })
-
+  
   output$grafica_barras <- renderPlot({
     gen_barras(edo_sel = input$selEnt,
                ind_sel = input$selIndicador,
@@ -67,41 +120,7 @@ server <- function(input, output, session) {
                ind_sel = input$selIndicador,
                anio_sel = input$sldAnio)
   })
-
+  
 }
-
-ui <- fluidPage(
-  h1("Catálogo de indicadores", style = "color:rgb(0,167,225);"),
-  #br(),
-  sidebarLayout(
-    sidebarPanel(
-      #      h2("Controles"),
-      selectInput(inputId = "selColeccion",
-                  label = "Colección:",
-                  choices = opciones_coleccion,
-                  selected = opciones_coleccion[1]),
-      selectInput(inputId = "selIndicador",
-                  label = "Indicador:",
-                  choices = opciones_indicadores,
-                  selected = opciones_indicadores[1]),
-      radioButtons(inputId = "selEnt",
-                  label = "Desagregación:",
-                  choices = opciones_entidad),
-      uiOutput("sldAnio"),
-      br(),
-      helpText("© 2023 Gobierno del Estado de Guanajuato")
-    ),
-    mainPanel(
-      tabsetPanel(
-        tabPanel("Gráfica",
-                 plotOutput("grafica_barras", height = "80vh") %>% withSpinner()),
-        tabPanel("Mapa",
-                 plotOutput("mapa", height = "80vh") %>% withSpinner()),
-        tabPanel("Serie",
-                 plotOutput("grafica_lineas", height = "80vh") %>% withSpinner())
-      )
-    )
-  )
-)
 
 shinyApp(ui, server)
