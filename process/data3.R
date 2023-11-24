125ea3bd9d341ce7f70e35cf959b4afba3d4fc86
mario <azulmario@yahoo.com.mx>
2023-11-24 17:04:23 -0600

    Incorpora datos actualizados del PIB

--- process/data3.R
+++ process/data3.R
@@ -13,13 +13,20 @@ con <- dbConnect(odbc::odbc(), "circinus", timeout = 10)
 # Página web o sitio de consulta:
 # https://www.inegi.org.mx/programas/pibent/2018/#datos_abiertos
 # https://www.inegi.org.mx/contenidos/programas/pibent/2018/datosabiertos/piber_datos_abiertos_csv.zip
-data <- "/media/mario/home/R/PIBe/piber.csv"
+# data_file <- "/media/mario/home/R/PIBe/piber.csv"
+# data_file <- "/media/mario/home/R/PIBe/piber_percapita.csv"
+# data_file <- "/media/mario/home/R/PIBe/piber_percapita_log.csv"
+# data <- read_csv(data_file)
+# dbWriteTable(con, "tabla_piber_pclog", data, overwrite = TRUE, row.names = FALSE)
 
-# Read the data from CSV file
-piber_data <- read_csv(data)
+# mnemonico_data <- read_csv("process/mnemonico.csv")
+# dbWriteTable(con, "mnemonico", mnemonico_data, append = TRUE, row.names = FALSE)
 
-# Use dbWriteTable to upload the data
-dbWriteTable(con, "tabla_piber", piber_data, overwrite = TRUE, row.names = FALSE)
+# indicador_data <- read_csv("process/indicador.csv")
+# dbWriteTable(con, "indicador", indicador_data, append = TRUE, row.names = FALSE)
+
+serie_data <- read_csv("process/serie.csv")
+dbWriteTable(con, "serie", serie_data, append = TRUE, row.names = FALSE)
 
 # Close the connection
 dbDisconnect(con)
